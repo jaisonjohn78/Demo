876e9a7733a685ba219f16b85117804e64f4f465
Jaisonjohn78 <jaisonjohn78.com@gmail.com>
2022-06-01 00:23:58 +0530

    Database setup

--- config.php
+++ config.php
@@ -1,8 +1,13 @@
 <?php
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "peradot";
+// $hostname = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "peradot";
+
+$hostname = "peradot.in";
+$username = "u230766858_peradot";
+$password = "Peradot@1";
+$database = "u230766858_peradot";
 
 
 $con = mysqli_connect($hostname, $username, $password, $database) or die("Database connection failed");
