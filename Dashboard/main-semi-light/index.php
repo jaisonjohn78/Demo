a628cbc5af10d550c90c134d9d728b5147115b3c
Jaisonjohn78 <jaisonjohn78.com@gmail.com>
2022-06-02 13:29:30 +0530

    Multiple Timer set

--- Dashboard/main-semi-light/index.php
+++ Dashboard/main-semi-light/index.php
@@ -37,8 +37,6 @@ if(isset($_POST['claim'])){
     mysqli_query($con, "UPDATE package SET days = days - 1, timestamp = '$next_claim' where user_id = $id");
     mysqli_query($con, "UPDATE users SET amount = amount + '$reward' where id = $id");
     redirect('index.php');
-
-
 }
  
 
@@ -537,10 +535,15 @@ if(isset($_POST['package5'])){
                                         <div>
                     
                                             <?php
-                                                
-                                                if(mysqli_num_rows($check_package_id)){
+                                            $i = 1;
+                                                while($claim_btns = mysqli_fetch_assoc($check_package_id))
+                                                {
+                                        
+                                                    if(mysqli_num_rows($check_package_id))
+                                                    {
                                                     
-                                                    if(strtotime($today) >= strtotime($check_time) ) {
+                                                    if(strtotime($today) >= strtotime($check_time) ) 
+                                                        {
                                                     
                                             ?>
                                                     <form method="POST">
@@ -554,9 +557,12 @@ if(isset($_POST['package5'])){
                                             
                                                 ?>
                                             
-                                                    <h2 id='demo'>24 : 00 : 00</h2>
+                                                    <h2 id='demo<?php echo $i ?>'>24 : 00 : 00</h2>
                                                     <h5>hr : min: sec</h5>
                                             <?php 
+                                                
+                                                $i = $i + 1;
+                                                
                                                } 
                                             }
                                                 else{
@@ -566,6 +572,7 @@ if(isset($_POST['package5'])){
                                                     </center>
                                                     <?php
                                                 }
+                                            }
                                             
                                             ?>
                                         </div>
@@ -1249,31 +1256,91 @@ if(isset($_POST['package5'])){
                             window.history.replaceState( null, null, window.location.href );
                         }
                     // Set the date we're counting down to
-                    var countDownDate = new Date("<?php echo $check_time ?>").getTime();
+                    var countDownDate1 = new Date("<?php echo $check_time ?>").getTime();
 
                     // Update the count down every 1 second
-                    var x = setInterval(function() {
+                    var x1 = setInterval(function() {
 
                         // Get today's date and time
                         var now = new Date().getTime();
 
                         // Find the distance between now and the count down date
-                        var distance = countDownDate - now;
+                        var distance1 = countDownDate1 - now;
 
                         // Time calculations for days, hours, minutes and seconds
-                        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-                        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                        var days = Math.floor(distance1 / (1000 * 60 * 60 * 24));
+                        var hours = Math.floor((distance1 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                        var minutes = Math.floor((distance1 % (1000 * 60 * 60)) / (1000 * 60));
+                        var seconds = Math.floor((distance1 % (1000 * 60)) / 1000);
 
                         // Output the result in an element with id="demo"
-                        document.getElementById("demo").innerHTML = hours + "h " +
+                        document.getElementById("demo1").innerHTML = hours + "h " +
                             minutes + "m " + seconds + "s ";
 
                         // If the count down is over, write some text 
-                        if (distance < 0) {
+                        if (distance1 < 0) {
                             clearInterval(x);
-                            document.getElementById("demo").innerHTML = "EXPIRED";
+                            document.getElementById("demo1").innerHTML = "EXPIRED";
+                        }
+                    }, 1000);
+                    </script>
+
+
+                    <script>
+                    // Set the date we're counting down to
+                    var countDownDate2 = new Date("<?php echo $check_time ?>").getTime();
+
+                    // Update the count down every 1 second
+                    var x2 = setInterval(function() {
+
+                        
+                        var now = new Date().getTime();
+                        // Find the distance between now and the count down date
+                        var distance2 = countDownDate2 - now;
+
+                        // Time calculations for days, hours, minutes and seconds
+                        var days = Math.floor(distance2 / (1000 * 60 * 60 * 24));
+                        var hours = Math.floor((distance2 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                        var minutes = Math.floor((distance2 % (1000 * 60 * 60)) / (1000 * 60));
+                        var seconds = Math.floor((distance2 % (1000 * 60)) / 1000);
+
+                        // Output the result in an element with id="demo"
+                        document.getElementById("demo2").innerHTML = hours + "h " +
+                            minutes + "m " + seconds + "s ";
+
+                        // If the count down is over, write some text 
+                        if (distance2 < 0) {
+                            clearInterval(x2);
+                            document.getElementById("demo2").innerHTML = "EXPIRED";
+                        }
+                    }, 1000);
+                    </script>
+
+
+                    <script>
+                    // Set the date we're counting down to
+                    var countDownDate3 = new Date("<?php echo $check_time ?>").getTime();
+
+                    // Update the count down every 1 second
+                    var x3 = setInterval(function() {
+                        var now = new Date().getTime();
+                        // Find the distance between now and the count down date
+                        var distance3 = countDownDate3 - now;
+
+                        // Time calculations for days, hours, minutes and seconds
+                        var days = Math.floor(distance3 / (1000 * 60 * 60 * 24));
+                        var hours = Math.floor((distance3 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                        var minutes = Math.floor((distance3 % (1000 * 60 * 60)) / (1000 * 60));
+                        var seconds = Math.floor((distance3 % (1000 * 60)) / 1000);
+
+                        // Output the result in an element with id="demo"
+                        document.getElementById("demo3").innerHTML = hours + "h " +
+                            minutes + "m " + seconds + "s ";
+
+                        // If the count down is over, write some text 
+                        if (distance3 < 0) {
+                            clearInterval(x3);
+                            document.getElementById("demo3").innerHTML = "EXPIRED";
                         }
                     }, 1000);
                     </script>
