9c03ef87eead0560e5706889a63220ca1c2146e6
Jaisonjohn78 <jaisonjohn78.com@gmail.com>
2022-06-03 21:46:09 +0530

    Database Renewed

--- Database/users.sql
+++ Database/users.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 31, 2022 at 06:32 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.5
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jun 03, 2022 at 04:15 PM
+-- Server version: 10.5.15-MariaDB-cll-lve
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `peradot`
+-- Database: `u230766858_peradot`
 --
 
 -- --------------------------------------------------------
@@ -42,19 +42,22 @@ CREATE TABLE `users` (
   `days` int(11) NOT NULL,
   `amount` int(11) NOT NULL DEFAULT 0,
   `reference_id` varchar(255) NOT NULL,
-  `admin` enum('0','1') NOT NULL DEFAULT '0'
+  `admin` enum('0','1') NOT NULL DEFAULT '0',
+  `code` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `phone`, `password`, `status`, `timestamp`, `metamask`, `withdraw`, `deposit`, `reward`, `days`, `amount`, `reference_id`, `admin`) VALUES
-(1, 'jaison', 'jaison@gmail.com', '9016368471', '202cb962ac59075b964b07152d234b70', '1', '', '', 0, 0, 0, 0, 0, 'MPbDjOG92Q', '1'),
-(3, 'hardik', 'hardikzz0409@gmail.com', '9016368472', '202cb962ac59075b964b07152d234b70', '1', '', '', 1000, 1500, 0, 0, 18527, 'isNmfqFQY0', '0'),
-(4, 'test1', 't1@gmail.com', '9594604829', '202cb962ac59075b964b07152d234b70', '1', 'May 30, 2022, 1:23 am', '', 0, 0, 0, 0, 880, 'DdGv4wZX', '0'),
-(5, 'test2', 't2@gmail.com', '1239999999', '202cb962ac59075b964b07152d234b70', '1', 'May 30, 2022, 1:24 am', '', 0, 0, 0, 0, 600, 'CUwEWTkA', '0'),
-(6, 'nikhil', 'n@gmail.com', '9016368478', '202cb962ac59075b964b07152d234b70', '1', 'May 31, 2022, 9:43 pm', '', 0, 0, 0, 0, 0, 'RuAB2MC0', '0');
+INSERT INTO `users` (`id`, `username`, `email`, `phone`, `password`, `status`, `timestamp`, `metamask`, `withdraw`, `deposit`, `reward`, `days`, `amount`, `reference_id`, `admin`, `code`) VALUES
+(1, 'Peradot Foundation', 'peradotfoundation@gmail.com', '9999999999', 'e3ad8800e87ef74d996c5afed87471a1', '1', 'June 1, 2022, 12:34 am', '', 0, 0, 0, 0, 0, 'oldtvOHh', '1', ''),
+(2, 'Jaison John ', 'jaisonjohn78.com@gmail.com', '8693018540', 'e570ee21fcc2fdb1327a34158fc452e7', '1', 'June 1, 2022, 12:38 am', '', 0, 27000, 0, 0, 180, 'aFA15ETv', '0', ''),
+(3, 'Hardik vekariya', 'hardikzz0409@gmail.com', '9016368472', 'c4ca4238a0b923820dcc509a6f75849b', '1', 'June 1, 2022, 8:50 am', '', 0, 0, 0, 0, 0, 'AzfEcveL', '0', ''),
+(6, 'Test4', 'joysonjohnn@gmail.com', '+91 9594664829', '202cb962ac59075b964b07152d234b70', '1', 'June 3, 2022, 3:50 pm', '', 0, 0, 0, 0, 0, 'vAjDt1Hb', '0', ''),
+(7, 'Hp123', 'fackf0343@gmail.com', '9313412709', 'd4395a5856617fa4afe8c5cd2eed3912', '1', 'June 3, 2022, 4:08 pm', '', 0, 0, 0, 0, 0, 'Ry58Y2t6', '0', 'e87cc4b4e3c9a127350bd0f204884879'),
+(8, 'Vdking', 'maniyavishal42@gmail.com', '9824444139', 'd4395a5856617fa4afe8c5cd2eed3912', '1', 'June 3, 2022, 4:09 pm', '', 0, 3000, 0, 0, 0, '0HUxsaCf', '0', ''),
+(9, 'herry', 'patelherry2309@gmail.com', '9979552794', 'd4395a5856617fa4afe8c5cd2eed3912', '1', 'June 3, 2022, 5:12 pm', '', 0, 0, 0, 0, 0, 'denY53ok', '0', '');
 
 --
 -- Indexes for dumped tables
@@ -74,7 +77,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- Database/withdraw.sql
+++ Database/withdraw.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 31, 2022 at 06:32 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.5
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jun 03, 2022 at 04:15 PM
+-- Server version: 10.5.15-MariaDB-cll-lve
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `peradot`
+-- Database: `u230766858_peradot`
 --
 
 -- --------------------------------------------------------
@@ -54,7 +54,7 @@ ALTER TABLE `withdraw`
 -- AUTO_INCREMENT for table `withdraw`
 --
 ALTER TABLE `withdraw`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
