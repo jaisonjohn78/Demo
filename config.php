612aaba42b5a8557f21c70ff657d3bfdcdce2ad9
Jaisonjohn78 <jaisonjohn78.com@gmail.com>
2022-06-10 21:03:03 +0530

    Final deployment changes

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "peradot";
+// $hostname = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "peradot";
 
-// $hostname = "127.0.0.1:3306";
-// $username = "u230766858_peradot";
-// $password = "Peradot@1";
-// $database = "u230766858_peradot";
+$hostname = "127.0.0.1:3306";
+$username = "u230766858_peradot";
+$password = "Peradot@1";
+$database = "u230766858_peradot";
 
 
 $con = mysqli_connect($hostname, $username, $password, $database) or die("Database connection failed");
